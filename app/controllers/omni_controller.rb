c4c8d82fa10db13dff3df9df67713b6884e870f1
gabrielcroitoru97 <gabrielcroitoru@hotmail.com>
2024-07-10 03:14:59 +0000

    final commit

--- app/controllers/omni_controller.rb
+++ app/controllers/omni_controller.rb
@@ -20,12 +20,27 @@ class OmniController < ApplicationController
     render(:template=>"omni_templates/square_results")
   end
   def square_root_results
+    @sqrtnum=params.fetch("sqrtnum").to_d
+    @rooted=@sqrtnum**0.5
     render(:template=>"omni_templates/square_root_results")
   end
   def payment_results
+    @apr=params.fetch("apr").to_f
+    @apr_per=@apr.to_fs(:percentage, { :precision => 4 } ) 
+    @num_years=params.fetch("num_years").to_i
+    prin=params.fetch("principal").to_d
+    @principal=prin.to_fs(:currency) 
+    per = @apr*0.01/12
+    months=@num_years*12
+    pay=(per*prin)/(1-((1+per)**(months*-1)))
+    @payment=pay.to_fs(:currency) #"$"+number_with_delimiter(pay.round(2), :delimiter => ',').to_s
     render(:template=>"omni_templates/payment_results")
   end
+
   def random_results
+    @min=params.fetch("min").to_d
+    @max=params.fetch("max").to_d
+    @rand=rand(@min..@max)
     render(:template=>"omni_templates/random_results")
   end
 end
